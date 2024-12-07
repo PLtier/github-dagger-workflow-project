a17b475a2bbcaf4941d761303c143cc0bf31cbda
Maciej Jalocha <maciej.jalocha.pl@gmail.com>
2024-12-07 16:04:27 +0100

    feat(pipeline): install pip deps from file

--- pipeline.go
+++ pipeline.go
@@ -22,19 +22,24 @@ func main() {
 	ls(ctx, container)
 	// _, err = python.File("../artifacts/lead_model_lr.pkl").Export(ctx, "model.pkl")
 	container = copyCode(client, container)
-	container = container.WithWorkdir("/pipeline")
-	ls(ctx, container)
+	container = installDeps(container)
 }
 
 func copyCode(client *dagger.Client, container *dagger.Container) *dagger.Container {
 	container = container.
 		WithDirectory("/pipeline/.dvc", client.Host().Directory(".dvc")).
 		WithDirectory("/pipeline/.git", client.Host().Directory(".git")).
+		WithFile("/pipeline/requirements.txt", client.Host().File("pipeline_deps/requirements.txt")).
 		WithDirectory("/pipeline/github_dagger_workflow_project", client.Host().Directory("github_dagger_workflow_project"))
 	return container
 }
 
-func ls(ctx context.Context, container *dagger.Container) {
+func installDeps(container *dagger.Container) *dagger.Container {
+	container = container.WithExec([]string{"pip", "install", "-r", "pipeline/requirements.txt"})
+	return container
+}
+
+func ls(ctx context.Context, container *dagger.Container, message string) {
 	info, err := container.WithExec([]string{"ls", "-la"}).Stdout(ctx)
 	if err != nil {
 		fmt.Println(err)
