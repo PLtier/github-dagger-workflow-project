83d35fed381a7924c54b303c0462c4d48e4a9190
swae <swae@itu.dk>
2024-12-08 18:43:00 +0100

    refactor: removed print statements and organised imports

--- github_dagger_workflow_project/data_transformations.py
+++ github_dagger_workflow_project/data_transformations.py
@@ -1,38 +1,16 @@
 import utils
-# dbutils.widgets.text("Training data max date", "2024-01-31")
-# dbutils.widgets.text("Training data min date", "2024-01-01")
-# max_date = dbutils.widgets.get("Training data max date")
-# min_date = dbutils.widgets.get("Training data min date")
-
-# testnng
-max_date = "2024-01-31"
-min_date = "2024-01-01"
-
 import os
-import shutil
-from pprint import pprint
-
-# shutil.rmtree("./artifacts",ignore_errors=True)
-os.makedirs("artifacts",exist_ok=True)
-print("Created artifacts directory")
-
 import pandas as pd
 import warnings
-
-warnings.filterwarnings('ignore')
-pd.set_option('display.float_format',lambda x: "%.3f" % x)
-
-print("Loading training data")
-
-data = pd.read_csv("./artifacts/raw_data.csv")
-""
-
-print("Total rows:", data.count())
-# display(data.head(5))
-
-import pandas as pd
 import datetime
 import json
+import numpy as np
+from sklearn.preprocessing import MinMaxScaler
+import joblib
+
+# Define min and max date
+max_date = "2024-01-31"
+min_date = "2024-01-01"
 
 if not max_date:
     max_date = pd.to_datetime(datetime.datetime.now().date()).date()
@@ -40,6 +18,16 @@ else:
     max_date = pd.to_datetime(max_date).date()
 
 min_date = pd.to_datetime(min_date).date()
+
+# Create artifacts folder
+os.makedirs("artifacts",exist_ok=True)
+
+# Warnings and pandas settings
+warnings.filterwarnings('ignore')
+pd.set_option('display.float_format',lambda x: "%.3f" % x)
+
+# Loading raw data
+data = pd.read_csv("./artifacts/raw_data.csv")
 
 # Time limit data
 data["date_part"] = pd.to_datetime(data["date_part"]).dt.date
@@ -64,8 +52,6 @@ data = data.drop(
     axis=1
 )
 
-import numpy as np
-
 data["lead_indicator"].replace("", np.nan, inplace=True)
 data["lead_id"].replace("", np.nan, inplace=True)
 data["customer_code"].replace("", np.nan, inplace=True)
@@ -76,10 +62,8 @@ data = data.dropna(axis=0, subset=["lead_id"])
 data = data[data.source == "signup"]
 result=data.lead_indicator.value_counts(normalize = True)
 
-print("Target value counter")
 for val, n in zip(result.index, result):
     print(val, ": ", n)
-data
 
 vars = [
     "lead_id", "lead_indicator", "customer_group", "onboarding", "source", "customer_code"
@@ -87,25 +71,18 @@ vars = [
 
 for col in vars:
     data[col] = data[col].astype("object")
-    print(f"Changed {col} to object type")
 
 cont_vars = data.loc[:, ((data.dtypes=="float64")|(data.dtypes=="int64"))]
 cat_vars = data.loc[:, (data.dtypes=="object")]
 
-print("\nContinuous columns: \n")
-pprint(list(cont_vars.columns), indent=4)
-print("\n Categorical columns: \n")
-pprint(list(cat_vars.columns), indent=4)
+cont_vars = cont_vars.apply(lambda x: x.clip(lower = (x.mean() - 2*x.std()),
+                                             upper = (x.mean() + 2*x.std())))
 
-cont_vars = cont_vars.apply(lambda x: x.clip(lower = (x.mean()-2*x.std()),
-                                             upper = (x.mean()+2*x.std())))
 outlier_summary = cont_vars.apply(utils.describe_numeric_col).T
 outlier_summary.to_csv('./artifacts/outlier_summary.csv')
-outlier_summary
 
 cat_missing_impute = cat_vars.mode(numeric_only=False, dropna=True)
 cat_missing_impute.to_csv("./artifacts/cat_missing_impute.csv")
-cat_missing_impute
 
 # Continuous variables missing values
 cont_vars = cont_vars.apply(utils.impute_missing_values)
@@ -114,37 +91,27 @@ cont_vars.apply(utils.describe_numeric_col).T
 cat_vars.loc[cat_vars['customer_code'].isna(),'customer_code'] = 'None'
 cat_vars = cat_vars.apply(utils.impute_missing_values)
 cat_vars.apply(lambda x: pd.Series([x.count(), x.isnull().sum()], index = ['Count', 'Missing'])).T
-cat_vars
 
-from sklearn.preprocessing import MinMaxScaler
-import joblib
-
+# Scaling continuous variables
 scaler_path = "./artifacts/scaler.pkl"
-
 scaler = MinMaxScaler()
 scaler.fit(cont_vars)
 
 joblib.dump(value=scaler, filename=scaler_path)
-print("Saved scaler in artifacts")
 
 cont_vars = pd.DataFrame(scaler.transform(cont_vars), columns=cont_vars.columns)
-cont_vars
-
 cont_vars = cont_vars.reset_index(drop=True)
 cat_vars = cat_vars.reset_index(drop=True)
+
+# Concatenating the categorical and continuous variables
 data = pd.concat([cat_vars, cont_vars], axis=1)
-print(f"Data cleansed and combined.\nRows: {len(data)}")
-data
-
-import json
-
 data_columns = list(data.columns)
-with open('./artifacts/columns_drift.json','w+') as f:           
-    json.dump(data_columns,f)
-    
-data.to_csv('./artifacts/training_data.csv', index=False)
 
-data.columns
+with open('./artifacts/columns_drift.json', 'w+') as f:           
+    json.dump(data_columns, f)
+
+# Storing the final training data
+data.to_csv('./artifacts/training_data.csv', index=False)
 
 data['bin_source'] = data['source']
 values_list = ['li', 'organic','signup','fb']
@@ -156,11 +123,5 @@ mapping = {'li' : 'socials',
            }
 
 data['bin_source'] = data['source'].map(mapping)
-
-#spark.sql(f"drop table if exists train_gold")
-
-# data_gold = spark.createDataFrame(data)
-# data_gold.write.saveAsTable('train_gold')
-# dbutils.notebook.exit(('training_golden_data',most_recent_date))
 
 data.to_csv('./artifacts/train_data_gold.csv', index=False)
