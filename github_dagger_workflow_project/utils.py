a72688f29097d4b984143c886d1e436753f80b17
swae <swae@itu.dk>
2024-12-08 18:04:12 +0100

    refactor: utils script to store function definitions

---
+++ github_dagger_workflow_project/utils.py
@@ -0,0 +1,39 @@
+def describe_numeric_col(x):
+    """
+    Parameters:
+        x (pd.Series): Pandas col to describe.
+    Output:
+        y (pd.Series): Pandas series with descriptive stats. 
+    """
+    return pd.Series(
+        [x.count(), x.isnull().count(), x.mean(), x.min(), x.max()],
+        index=["Count", "Missing", "Mean", "Min", "Max"]
+    )
+
+
+def impute_missing_values(x, method="mean"):
+    """
+    Parameters:
+        x (pd.Series): Pandas col to describe.
+        method (str): Values: "mean", "median"
+    """
+    if (x.dtype == "float64") | (x.dtype == "int64"):
+        x = x.fillna(x.mean()) if method=="mean" else x.fillna(x.median())
+    else:
+        x = x.fillna(x.mode()[0])
+    return x
+
+
+def wait_for_deployment(model_name, model_version, stage='Staging'):
+    status = False
+    while not status:
+        model_version_details = dict(
+            client.get_model_version(name=model_name,version=model_version)
+            )
+        if model_version_details['current_stage'] == stage:
+            print(f'Transition completed to {stage}')
+            status = True
+            break
+        else:
+            time.sleep(2)
+    return status
