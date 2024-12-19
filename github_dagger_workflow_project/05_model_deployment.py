197068550560bdbd712b7252890476bb92da3cac
swae <swae@itu.dk>
2024-12-19 10:48:48 +0100

    refactor: moved function definitions to pipeline_utils and constants to config

--- github_dagger_workflow_project/05_model_deployment.py
+++ github_dagger_workflow_project/05_model_deployment.py
@@ -1,18 +1,20 @@
 from github_dagger_workflow_project import utils
 from github_dagger_workflow_project.mlflow_client import client
+from github_dagger_workflow_project.config import (
+    MODEL_NAME,
+    MODEL_VERSION,
+)
 
-model_version = 1
-model_name = "lead_model"
+
 model_status = True
 
-
-model_version_details = dict(client.get_model_version(name=model_name, version=model_version))
+model_version_details = dict(client.get_model_version(name=MODEL_NAME, version=MODEL_VERSION))
 
 if model_version_details["current_stage"] != "Staging":
     client.transition_model_version_stage(
-        name=model_name,
-        version=model_version,
+        name=MODEL_NAME,
+        version=MODEL_VERSION,
         stage="Staging",
         archive_existing_versions=True,
     )
-    model_status = utils.wait_for_deployment(model_name, model_version, client, "Staging")
+    model_status = utils.wait_for_deployment(MODEL_NAME, MODEL_VERSION, client, "Staging")
